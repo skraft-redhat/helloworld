37e7555d90be90c5b090c7a2c3be151ca108422d
skraft-redhat <57144373+skraft-redhat@users.noreply.github.com>
2023-05-31 12:50:27 +0200

    Update GreetingResource.java

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -16,6 +16,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello World";
+        return "Hello new World";
     }
 }
